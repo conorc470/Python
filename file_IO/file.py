4bb1e5b618dae0c273f0c1d23dc96340349d681c
Conor Clarke <conorc470@gmail.com>
2018-01-16 18:01:09 +0000

    Changed File I/O

--- file_IO/file.py
+++ file_IO/file.py
@@ -55,7 +55,7 @@ for filename in os.listdir('C:\\Windows\\System32'):
 print(totalSize)
 
 # Reading & Writing files in Python
->>> baconFile = open('bacon.txt', 'w') # w method overwrites existing content
+>>> baconFile = open('bacon.txt', 'w') # .w method overwrites existing content
 >>> baconFile.write('Hello world!\n')
 >>> baconFile.close()
 >>> baconFile = open('bacon.txt', 'a') # a method appends on to the end of existing text
@@ -68,7 +68,77 @@ print(totalSize)
 
 .readlines() # gets list of string values from the file, one string for each line of text
 
+r       # read - Only allow reading from a file. If it doesn’t exist, there will be an error.
+w       # write - Only allow writing to a file. If it doesn’t exist, a new one will be created
+a       # append - Append to a file, e.g. write new content after the existing content. If it doesn’t exist, a new one will be created
+r+      # read and write - Allow reading and writing to a file.  If it doesn’t exist, there will be an error.
+w+      # read and write - Same as r+ but a new file will be created if it doesn’t exist
+rb/wb   # binary - Same as r/w but use binary instead of text (Windows only)
 
+f = open(‘questions.txt’)  # Because ‘r’ is used more often than the others, you can use open without the last argument to mean read-only
 
+# WITH - Instead of remembering to close the file, the file is closed automatically when leaving the block of code within the ‘with’ clause.
+with open('questions.txt') as f:
+    lines = f.readlines()
+print lines
 
+# SHUTIL
+import shutil, os  # shutil provides functions for copying files and folders
+>>> os.chdir('C:\\')
+u >>> shutil.copy('C:\\spam.txt', 'C:\\delicious') #(source, destination)
+'C:\\delicious\\spam.txt'
+v >>> shutil.copy('eggs.txt', 'C:\\delicious\\eggs2.txt') #if destination is a filename, its used as new name of copied file
+'C:\\delicious\\eggs2.txt'
 
+shutil.copy()       #copy a single file     
+shutil.copytree()   #copy an entire folder and everything in it
+shutil.move()       #move file or folder a path source to destination
+os.unlink()         #delete the file at path
+os.rmdir()          #delete the folder at path but folder must be empty beforehand
+shutil.rmtree()     #delete the folder at path and all files and folders will also be deleted
+
+# Walking A Directory Tree
+import os
+for folderName, subfolders, filenames in os.walk('C:\\delicious'):
+    print('The current folder is ' + folderName)
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+    for filename in filenames:
+        print('FILE INSIDE ' + folderName + ': '+ filename)
+    print('')
+'The current folder is C:\delicious'  # A string of the current folder’s name
+'SUBFOLDER OF C:\delicious: cats'     # A list of strings of the folders in the current folder
+'SUBFOLDER OF C:\delicious: walnut'  
+'FILE INSIDE C:\delicious: spam.txt'  # A list of strings of the files in the current folder
+
+# ZIP FILES
+>>> import zipfile, os
+>>> os.chdir('C:\\') #move to the folder with example.zip
+>>> exampleZip = zipfile.ZipFile('example.zip')
+>>> exampleZip.namelist() #returns list of strings for all files and folders contained in zip file.
+['spam.txt', 'cats/', 'cats/catnames.txt', 'cats/zophie.jpg']
+>>> spamInfo = exampleZip.getinfo('spam.txt') #.getinfo returns a ZipInfo object about that file
+>>> spamInfo.file_size 
+13908
+>>> spamInfo.compress_size
+3828
+
+# Extracting from ZIP files
+>>> import zipfile, os
+>>> os.chdir('C:\\') # move to the folder with example.zip
+>>> exampleZip = zipfile.ZipFile('example.zip')
+>>> exampleZip.extractall()
+>>> exampleZip.close()
+
+# Creating and adding to ZIP Files
+>>> import zipfile
+>>> newZip = zipfile.ZipFile('new.zip', 'w')
+>>> newZip.write('spam.txt', compress_type=zipfile.ZIP_DEFLATED)
+>>> newZip.close()
+# This code creates new ZIP file called new.zip that has the compressed contents of spam.txt
+# Write mode will erase existing contents of a ZIP file
+# Use append 'a' to simply add files to existing ZIP file.
+
+Note
+# When writing programs that handle files, it’s a good idea to comment out the code that does the actual 
+# copy/move/rename/delete and add a print() call instead so you can run the program and verify exactly what it will do.
